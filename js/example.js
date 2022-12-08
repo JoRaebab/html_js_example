ddb35836133d40ebf06edd4fa8b2ee43e5ee67cd
Jo Seung Hoon <buzzi123@naver.com>
2022-12-08 16:10:13 +0900

    이름, 학번, 이메일 화면 반영 및 새로고침 값 유지

--- js/example.js
+++ js/example.js
@@ -1,18 +1,60 @@
 const nameH1element = document.querySelector('h1.inline');
 const connectNameElement = document.querySelector('span.red');
+const studentNoElement = document.querySelector('span.studentNo');
+const emailElement = document.querySelector('span.email');
 
-const localName = localStorage.getItem('name');
-if (localName) {
-  nameH1element.textContent = localName;
-  connectNameElement.textContent = localName;
-}
+const inputModalElement = document.querySelector('#inputModal');
+
+const setUserName = (userName) => {
+  nameH1element.textContent = userName;
+  connectNameElement.textContent = userName;
+};
+
+const setStudentNo = (studentNo) => {
+  studentNoElement.textContent = studentNo;
+};
+
+const setEmail = (email) => {
+  emailElement.textContent = email;
+};
+
+const localName = localStorage.getItem('userName');
+if (localName) setUserName(localName);
+
+const localStudentNo = localStorage.getItem('studentNo');
+if (localStudentNo) setStudentNo(localStudentNo);
+
+const localEmail = localStorage.getItem('email');
+if (localEmail) setEmail(localEmail);
+
 nameH1element.onclick = () => {
-  const inputName = prompt('이름을 입력해 주세요.');
-  if (inputName) {
-    localStorage.setItem('name', inputName);
-    nameH1element.textContent = inputName;
-    connectNameElement.textContent = inputName;
-  } else {
-    alert('이름이 입력되지 않았습니다.');
+  inputModalElement.showModal();
+  // const inputName = prompt('이름을 입력해 주세요.');
+  // if (inputName) {
+  //   localStorage.setItem('name', inputName);
+  //   setUserName(inputName);
+  // } else {
+  //   alert('이름이 입력되지 않았습니다.');
+  // }
+};
+
+const modalSubmitBtn = document.querySelector('button.modalSubmit');
+
+modalSubmitBtn.onclick = () => {
+  const modalFormElement = document.querySelector('.modalForm');
+  const formData = new FormData(modalFormElement);
+
+  // 새로고침해도 값 유지
+  for (const [key, value] of formData) {
+    localStorage.setItem(key, value);
+    if (key==='userName') setUserName(value);
+    if (key==='studentNo') setStudentNo(value);
+    if (key==='email') setEmail(value);
   }
+
+  inputModalElement.close();
+};
+
+inputModalElement.onclick = (event) => {
+  if (event.target.nodeName === 'DIALOG') inputModalElement.close();
 };
