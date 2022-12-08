10c228b06be54dd90c506daa6befad89c1bf7525
Jo Seung Hoon <buzzi123@naver.com>
2022-12-08 14:38:52 +0900

    이름 입력 및 접속자 메세지 추가

--- js/example.js
+++ js/example.js
@@ -1,3 +1,18 @@
-const test = 10;
+const nameH1element = document.querySelector('h1.inline');
+const connectNameElement = document.querySelector('span.red');
 
-console.log(test);
+const localName = localStorage.getItem('name');
+if (localName) {
+  nameH1element.textContent = localName;
+  connectNameElement.textContent = localName;
+}
+nameH1element.onclick = () => {
+  const inputName = prompt('이름을 입력해 주세요.');
+  if (inputName) {
+    localStorage.setItem('name', inputName);
+    nameH1element.textContent = inputName;
+    connectNameElement.textContent = inputName;
+  } else {
+    alert('이름이 입력되지 않았습니다.');
+  }
+};
